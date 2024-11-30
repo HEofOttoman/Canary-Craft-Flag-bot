72d04ec8b02e342af4c548f2a6d579d08af0f234
Hawoxaw <145900606+HEofOttoman@users.noreply.github.com>
2024-11-30 15:43:14 +1100

    v0.1.0 Beta
    The bot now understands commands and is functional.
    Should now work on getting it to send a message.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Canary Craft Flag Bot
 A discord bot that displays the flags of the countries in the Canary Craft/ADSCraft Minecraft server.
 
+Warning: Uses some privileged intents, and is thus not intended for large-scale use.
+
 ### Credits
 By HenryWauzivuff
 
 ### How to use
 Use the bot invite, and invite the bot in your wanted server
+
+To self-host, you must make a file called 'config.json' with a variable called `Token` with the value of your own bot token. 
--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-// Import necessary Discord.js
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-
-// Import the Token for the bot from the config
-const Token = require('./config.json')
-
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 const prefix = '!'
 
-bot.on('message', (message) => {
-    if (!message.content.startsWith(prefix)) return 
-    var command = message.content.slice(prefix.length)
-    if (command == 'hello') {
-        message.reply('world')
+// Create a new client (bot) instance
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers, // Privileged intent, find out how to use wihtout
+        GatewayIntentBits.MessageContent // Privileged intent, find out how to use without
+    ]
+});
+
+// When the client is ready, run this code (only once) to test if the bot is working
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Bot Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+    
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'hello') {
+        message.reply('Hey!');
     }
-})
+    if (message.content === '!Nordia') {
+        message.reply('The Standard of The Sublime Nordian Shogunate');
+    }
+});
 
-
-// Login into bot?s
-bot.login('Token')
+// Log in to Discord with your client's token
+client.login(token);
